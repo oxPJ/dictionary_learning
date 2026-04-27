1012ae0b80077920c31b65169beb8e9a973bd884
DJPJ <piotr.jedryszek@sjc.ox.ac.uk>
2026-04-27 21:04:06 +0100

    update init document so we only have the trainers we actually tested

--- dictionary_learning/trainers/__init__.py
+++ dictionary_learning/trainers/__init__.py
@@ -5,15 +5,10 @@ from .gated_anneal import GatedAnnealTrainer
 from .top_k import TopKTrainer
 from .jumprelu import JumpReluTrainer
 from .batch_top_k import BatchTopKTrainer, BatchTopKSAE
-from .jumprelu_l1 import JumpReluTrainerL1
-from .p_anneal_l2 import PAnnealTrainerL2
+from .top_k_l2 import TopKTrainerL2
 from .top_k_l1 import TopKTrainerL1
 from .batch_top_k_l1 import BatchTopKTrainerL1
 from .matryoshka_batch_top_k_l1 import MatryoshkaBatchTopKTrainerL1
-from .standard_l2 import StandardTrainerL2
-from .jumprelu_l2 import JumpReluTrainerL2
-from .p_anneal_l2w import PAnnealTrainerL2W
-from .top_k_l2 import TopKTrainerL2
 from .batch_top_k_l2 import BatchTopKTrainerL2
 from .matryoshka_batch_top_k_l2 import MatryoshkaBatchTopKTrainerL2
 
@@ -27,15 +22,12 @@ __all__ = [
     "JumpReluTrainer",
     "BatchTopKTrainer",
     "BatchTopKSAE",
-    "JumpReluTrainerL1",
-    "PAnnealTrainerL2",
+    "TopKTrainerL2",
     "TopKTrainerL1",
     "BatchTopKTrainerL1",
     "MatryoshkaBatchTopKTrainerL1",
-    "StandardTrainerL2",
-    "JumpReluTrainerL2",
-    "PAnnealTrainerL2W",
-    "TopKTrainerL2",
     "BatchTopKTrainerL2",
     "MatryoshkaBatchTopKTrainerL2",
 ]
+
+
